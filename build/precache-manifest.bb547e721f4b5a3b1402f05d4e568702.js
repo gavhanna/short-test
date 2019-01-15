1a1be527576e05c90add074ca3da630170be2539
Gav Hanna <gavhanna@gmail.com>
2019-01-15 20:46:27 +0000

    added build for hosting

---
+++ build/precache-manifest.bb547e721f4b5a3b1402f05d4e568702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1166f1e3f88b2dd19b37",
+    "url": "/static/js/main.1166f1e3.chunk.js"
+  },
+  {
+    "revision": "92162d40552153e90192",
+    "url": "/static/js/1.92162d40.chunk.js"
+  },
+  {
+    "revision": "1166f1e3f88b2dd19b37",
+    "url": "/static/css/main.2eed4faa.chunk.css"
+  },
+  {
+    "revision": "5d12f91a3f56a81ce48bc3e6aa013d4c",
+    "url": "/index.html"
+  }
+];
